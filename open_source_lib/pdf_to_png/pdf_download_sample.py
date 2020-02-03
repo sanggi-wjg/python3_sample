074053f2b70dfdacd763b4a952d7aa6a63af3f22
jay_g <jay_g@kr.accommate.com>
2020-02-03 16:00:53 +0900

    pdf to png 샘플

--- open_source_lib/pdf_to_png/pdf_download_sample.py
+++ open_source_lib/pdf_to_png/pdf_download_sample.py
@@ -2,6 +2,7 @@ import os
 import urllib.request
 
 import urllib.parse
+import urllib.error
 
 SAMPLE_PDF_IMAGE_URL = 'http://www.africau.edu/images/default/sample.pdf'
 SAMPLE_PDF_IMAGE_URL_2 = 'http://importwaybillfile.oss-cn-zhangjiakou.aliyuncs.com/imWaybill_LP00165786832283.pdf?Expires=1584367242&OSSAccessKeyId=LTAI5pw3uQoCjCRE&Signature=fLMr2YH9%2BJRNXWjP0A5zTEp%2B4P0%3D'
@@ -34,11 +35,19 @@ def ret_path_to_file_info(path: str, returnTo: str):
 
 
 if __name__ == '__main__':
-    urllib.request.urlretrieve(SAMPLE_PDF_IMAGE_URL_2, 'sample_waybill.pdf')
 
-    urlParse = ret_url_parse(SAMPLE_PDF_IMAGE_URL_2)
-    print(urlParse)
-    path = urlParse[2]
+    try:
+        urllib.request.urlretrieve(SAMPLE_PDF_IMAGE_URL_2, 'sample_waybill.pdf')
 
-    result = ret_path_to_file_info(path, 'filename')
-    print(result)
+        urlParse = ret_url_parse(SAMPLE_PDF_IMAGE_URL_2)
+        print(urlParse)
+        path = urlParse[2]
+
+        result = ret_path_to_file_info(path, 'filename')
+        print(result)
+
+    except urllib.error.HTTPError as e:
+        print('urllib.HTTPError Error (LINE) :', e.__traceback__.tb_lineno, ' (MSG) : ', e)
+
+    except Exception as e:
+        print('Exception Error (LINE) :', e.__traceback__.tb_lineno, ' (MSG) : ', e)
