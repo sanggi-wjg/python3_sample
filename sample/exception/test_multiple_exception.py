1ec70cd3c758e3a6ddba728a2874e6a91ab1ed8a
jay_g <jay_g@kr.accommate.com>
2020-02-25 11:15:41 +0900

    when raised exception, handle exception type

--- sample/exception/test_multiple_exception.py
+++ sample/exception/test_multiple_exception.py
@@ -1,11 +1,16 @@
-def raise_exception():
-    raise TypeError('Test Except')
-
-
 if __name__ == '__main__':
 
     try:
-        raise_exception()
+        raise TypeError('HA HA HA TEST TypeError')
+        # raise Exception('HA HA HA TEST Exception')
 
-    except (Exception, TypeError) as e:
-        print(e.args)
+    except (Exception, TypeError, NameError) as e:
+        print('@e.args@', e.args)
+
+        if isinstance(e, TypeError):
+            print('ok, is TypeError')
+
+        elif isinstance(e, Exception):
+            print('ok, is Exception')
+        else:
+            print('not ok')
