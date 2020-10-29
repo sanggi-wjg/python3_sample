3375ab99bcff9decddbe976f7ae06f633ed88dac
jay_g <jay_g@kr.accommate.com>
2020-10-29 17:20:52 +0900

    github open source : diagrams

---
+++ open_source_lib/diagram/diagram_sample.py
@@ -0,0 +1,10 @@
+from diagrams import Diagram
+from diagrams.aws.compute import EC2
+from diagrams.aws.database import RDS
+from diagrams.aws.network import ELB
+
+"""
+https://diagrams.mingrammer.com/docs/getting-started
+"""
+with Diagram("Web Service", show = False):
+    ELB("lb") >> EC2("web") >> RDS("userdb")
---
+++ sample/pytest/pytest_sample.py
@@ -0,0 +1,6 @@
+def sample_func(x):
+    return x + 1
+
+
+def test_answer():
+    assert sample_func(1) == 3
